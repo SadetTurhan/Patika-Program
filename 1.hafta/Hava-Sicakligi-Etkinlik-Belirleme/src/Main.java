0d67a3488410383b56a848e6326cab0d60bdab1d
SadetTurhan <msadetturhan1@gmail.com>
2023-12-14 16:27:25 +0300

    sicakliga gore etkinlik uygulamasi

---
+++ 1.hafta/Hava-Sicakligi-Etkinlik-Belirleme/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       int heat;
+
+       Scanner input = new Scanner(System.in);
+        System.out.println("Sıcaklığı giriniz: ");
+        heat = input.nextInt();
+
+        if (heat < 5){
+            System.out.println("Kayak yapabilirsiniz.");
+        }else if (heat <= 25){
+            if(heat <= 15){
+                System.out.println("Sinemaya gidebilirsiniz.");
+            }
+            if(heat >= 10){
+                System.out.println("Pinkiğe gidebilirsiniz.");
+            }
+        }else{
+            System.out.println("Yüzmeye gidebilirsiniz");
+        }
+    }
+}
