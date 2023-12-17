9bbb4e84e2df93f635cd2059c95e79bc5c5409c9
SadetTurhan <msadetturhan1@gmail.com>
2023-12-17 08:43:26 +0300

    Harmonik sayi bulma programi

---
+++ 1.hafta/Harmonik-Sayilari-Bulma/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("N sayısını giriniz: ");
+        int n = input.nextInt();
+        double result = 0.0;
+        for(int i = 1;i <= n; i++){
+            result += (1.0/i);
+        }
+        System.out.println(result);
+    }
+}
