9cf46b4df2b22821990990b046c7253ce7c2c713
SadetTurhan <msadetturhan1@gmail.com>
2023-12-19 18:05:20 +0300

    recursive ile uslu sayi alma eklendi

---
+++ 2.hafta/Recursive-Method-ile-Uslu-Sayi/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int usAlma(int a, int b) {
+        int sonuc = 1;
+        if (b >= 1) {
+            sonuc *= a;
+            return (sonuc * usAlma(a,b-1));
+        }
+        return sonuc;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Taban sayısını giriniz: ");
+        int a = input.nextInt();
+        System.out.println("Üs sayısını giriniz: ");
+        int b = input.nextInt();
+
+        int carpimSonucu = usAlma(a,b);
+        System.out.println(carpimSonucu);
+
+  }
+
+}
