b02e1e58e4e6f62cb9cf47987410abc80fc48a18
SadetTurhan <msadetturhan1@gmail.com>
2023-12-19 18:27:53 +0300

    Gelismis hesap makinesi eklendi

--- 2.hafta/Recursive-Method-ile-Uslu-Sayi/src/Main.java
+++ 2.hafta/Recursive-Method-ile-Uslu-Sayi/src/Main.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 
+
 public class Main {
-    public static int usAlma(int a, int b) {
-        int sonuc = 1;
-        if (b >= 1) {
-            sonuc *= a;
-            return (sonuc * usAlma(a,b-1));
-        }else if(b == 0){
-            return 1;
-        }
-        return sonuc;
-    }
-    public static void main(String[] args) {
+    public static void power(){
         Scanner input = new Scanner(System.in);
-        System.out.println("Taban sayısını giriniz: ");
+        System.out.print("Üssü Alınacak sayıyı giriniz : ");
         int a = input.nextInt();
-        System.out.println("Üs sayısını giriniz: ");
+
+        System.out.print("Üssü giriniz : ");
         int b = input.nextInt();
+        int result = 1;
 
-        int carpimSonucu = usAlma(a,b);
-        System.out.println(carpimSonucu);
+        for (int i = 1; i <= b; i++){
+            result *= a;
+        }
 
-  }
+        System.out.println("Sonuç : " + result);
 
+    }
+
+    public static void main(String[] args) {
+        power();
+    }
 }
