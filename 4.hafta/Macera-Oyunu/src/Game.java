c15ca5d1d1ed037e1a6e3395c812bd758d6779bf
SadetTurhan <msadetturhan1@gmail.com>
2024-01-04 17:03:42 +0300

    Karakter siniflari eklendi

---
+++ 4.hafta/Macera-Oyunu/src/Game.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Game {
+    private Scanner input = new Scanner(System.in);
+    public void start(){
+        System.out.println("Macera oyunua hoşgeldiniz !");
+        System.out.print("Lütfen bir isim giriniz : ");
+        String playerName = input.nextLine();
+        Player player = new Player(playerName);
+        System.out.println("Sayın " + player.getName() + " bu karanlık ve sisli adaya hoşgeldiniz ! Burada yaşananların hepsi gerçek !");
+        System.out.println("Lütfen bir karakter seçiniz !");
+        player.selectChar();
+    }
+}
---
+++ 4.hafta/Macera-Oyunu/src/Player.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class Player {
+    private int damage;
+    private int health;
+    private int money;
+    private String name;
+    private String charName;
+    private Scanner input = new Scanner(System.in);
+    public Player(String name){
+        this.name = name;
+    }
+    public void selectChar(){
+
+        GameChar[] charList = {new Samurai(),new Archer(),new Knight()};
+
+        System.out.println("------------------------------");
+        for(GameChar gameChar: charList){
+            System.out.println("ID : " + gameChar.getId() +
+                    "\t Karakter: " + gameChar.getName() +
+                    "\t\t Hasar: " + gameChar.getDamage() +
+                    "\t\t Sağlık: " + gameChar.getHealth() +
+                    "\t\t Para: " + gameChar.getMoney());
+        }
+        System.out.println("------------------------------");
+        System.out.println("Lütfen bir karakter seçiniz !");
+        int selectChar = input.nextInt();
+        switch (selectChar){
+            case 1:
+                initPlayer(new Samurai());
+                break;
+            case 2:
+                initPlayer(new Archer());
+                break;
+            case 3:
+                initPlayer(new Knight());
+                break;
+            default:
+                initPlayer(new Samurai());
+        }
+        System.out.println("Karakter : " + this.getCharName() +
+                "\t Hasar: " + this.getDamage() +
+                "\t Sağlık: " + this.getHealth() +
+                "\t Para: " + this.getMoney());
+    }
+    public void initPlayer(GameChar gameChar){
+            this.setDamage(gameChar.getDamage());
+            this.setHealth(gameChar.getHealth());
+            this.setMoney(gameChar.getMoney());
+            this.setCharName(gameChar.getName());
+    }
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCharName() {
+        return charName;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCharName(String charName) {
+        this.charName = charName;
+    }
+}
