cd8ee566412cae89ec4ba675b7186d379d30e4c5
SadetTurhan <msadetturhan1@gmail.com>
2024-01-17 18:44:13 +0300

    Harici metod eklendi

--- 5.hafta/Pratik-Try-Catch-Blogu/src/Main.java
+++ 5.hafta/Pratik-Try-Catch-Blogu/src/Main.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
+    public static int getArraysIndex(int []array, int index){
+        System.out.println(array[index]);
+        return array[index];
+    }
     public static void main(String[] args) {
         //initializing array
         int[] myArray = {5,10,15,20,25,30,35,40,45,50};
@@ -8,9 +12,10 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Lütfen aradığınız index'i giriniz");
         int arananIndex = input.nextInt();
+
         //try catch block
         try {
-            System.out.println(myArray[arananIndex]);
+            getArraysIndex(myArray,arananIndex);
         }catch(ArrayIndexOutOfBoundsException e){
             e.printStackTrace();
         }
